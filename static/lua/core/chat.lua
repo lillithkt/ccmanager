9fd246baa75e161d70f5b6f67f4ed2d61acabe78
Lillith Fox <imlvnaa@gmail.com>
2024-11-26 15:30:52 -0500

    pwetty pwease

--- static/lua/core/chat.lua
+++ static/lua/core/chat.lua
@@ -24,7 +24,7 @@ lvn.chat = {
       lvn.chat.send("xaero-waypoint:" .. name:gsub(":", "") .. ":" .. initials .. ":" .. x .. ":" .. y .. ":" .. z .. ":0:false:0:Internal-" .. dimension .. "-waypoints")
     else if config.waypointMode == "journey" then
       lvn.chat.send("[name:" .. name .. ",x:" .. x .. ",y:" .. y .. ",z:" .. z .. ",dim:" .. dimension .. "]")
-    else
+    else then
       lvn.chat.send("waypoint:" .. name .. ":" .. x .. ":" .. y .. ":" .. z .. ":" .. dimension)
     end
   end,
